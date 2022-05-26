6ba5c1e824cab11af741b04f646610aece1ce532
barharary <79626984+barharary@users.noreply.github.com>
2022-05-26 12:19:47 +0300

    Update CouponProject3Application.java

--- src/main/java/com/bh/CouponProject3/CouponProject3Application.java
+++ src/main/java/com/bh/CouponProject3/CouponProject3Application.java
@@ -14,31 +14,3 @@ public class CouponProject3Application {
 
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Learn: how to make :)
-// TODO Bean Person
-// TODO Swagger 3 dockers . not any.
-// TODO Eception with ENUM handler
-// TODO exception with other leng..
-// TODO changeCompanycoupons to coupons
-// TODO Filter java servlet filter
-// TODO Date automatic in Coupon bootstrap
-// TODO change name of methods name in advice.
-// TODO methods reference on token Job
-// TODO Aplication property without username and password. git ignore.
-// TODO image of Coupon to keep it in SpringCloud
